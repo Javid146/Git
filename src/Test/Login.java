64fcca500bf5df97eb619ae14d6cec4e13565fe3
Javid146 <mammadli.javid15@gmail.com>
2022-01-05 21:57:11 -0500

    update from intelliJ

--- src/Test/Login.java
+++ src/Test/Login.java
@@ -2,6 +2,10 @@ package Test;
 
 public class Login {
     public static void main(String[] args) {
+
+
+
         System.out.println("Hello World...");
+        System.out.println("Hello my dear Baku after 6 years.");
     }
 }
