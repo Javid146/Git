160d74c826f4d71a73024e7c155091a43129e929
Javid146 <mammadli.javid15@gmail.com>
2022-01-05 21:11:28 -0500

    UPDATE to logout

--- src/Test/Logout.java
+++ src/Test/Logout.java
@@ -3,6 +3,7 @@ package Test;
 public class Logout {
     public static void main(String[] args) {
         System.out.println("Hi bitches.");
+        System.out.println("Sorry for slang!");
     }
 
 }
